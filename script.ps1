3bc52488ff96c0af1d49d5f0ff8594e48786588a
Szalikko <91461505+Szalikko@users.noreply.github.com>
2023-09-18 17:35:34 +0200

    Create get_monitors_info function
    Function to get info about connected monitors

--- script.ps1
+++ script.ps1
@@ -1,14 +1,15 @@
 
-# function to scrape pc info
+# function to get pc info
 function get_pc_info {
     $manufacturer = (Get-WmiObject win32_bios | select Manufacturer | Format-Table -HideTableHeaders | Out-String).Trim()
     $model = (Get-CimInstance Win32_ComputerSystemProduct | Select Name | Format-Table -HideTableHeaders | Out-String).Trim()
     $serial = (Get-WmiObject win32_bios | select Serialnumber | Format-Table -HideTableHeaders | Out-String).Trim()
 
-    $result = "Manufacturer : $($manufacturer)`nModel        : $($model)`nSerial       : $($serial)`n------------------------------"
+    $result = "Manufacturer : $($manufacturer)`nModel        : $($model)`nSerial Number: $($serial)`n------------------------------"
                 
     return $result
 }
+
 # function to get user info
 function get_userinfo {
     $username = ($Env:UserName).Trim()
@@ -19,4 +20,24 @@ function get_userinfo {
 
     return $result
 
+}
+
+# function to get monitors info
+function get_monitors_info {
+    function Decode {
+        If ($args[0] -is [System.Array]) {
+            [System.Text.Encoding]::ASCII.GetString($args[0])
+        }
+        Else {
+            "Not Found"
+        }
+    }
+
+    ForEach ($Monitor in Get-WmiObject WmiMonitorID -Namespace root\wmi) {  
+        $Manufacturer = Decode $Monitor.ManufacturerName -notmatch 0
+        $Name = Decode $Monitor.UserFriendlyName -notmatch 0
+        $Serial = Decode $Monitor.SerialNumberID -notmatch 0
+        
+        echo "Manufacturer : $($Manufacturer)`nModel        : $($Name)`nSerial Number: $($Serial)`n"
+    }
 }
