5707a9eb5f0eec7f73882cf4ba3ba648b432c221
Szalikko <91461505+Szalikko@users.noreply.github.com>
2023-09-18 18:25:40 +0200

    Create output function
    Function to put everything into output.txt

--- script.ps1
+++ script.ps1
@@ -1,28 +1,26 @@
 
-# function to get pc info
+# get pc info
 function get_pc_info {
     $manufacturer = (Get-WmiObject win32_bios | Select-Object Manufacturer | Format-Table -HideTableHeaders | Out-String).Trim()
     $model = (Get-CimInstance Win32_ComputerSystemProduct | Select-Object Name | Format-Table -HideTableHeaders | Out-String).Trim()
     $serial = (Get-WmiObject win32_bios | Select-Object Serialnumber | Format-Table -HideTableHeaders | Out-String).Trim()
 
-    $result = "Manufacturer : $($manufacturer)`nModel        : $($model)`nSerial Number: $($serial)`n------------------------------"
-                
+    $result = "---------- Komputer ----------`nManufacturer : $($manufacturer)`nModel        : $($model)`nSerial Number: $($serial)`n`n------------------------------"
     return $result
 }
 
-# function to get user info
+# get user info
 function get_userinfo {
     $username = ($Env:UserName).Trim()
     $domain = ($Env:UserDomain).Trim()
     $pcname = ($Env:ComputerName).Trim()
 
     $result = "Username     : $($username)`nPCName       : $($pcname)`nDomain       : $($domain)`n"
-
     return $result
 
 }
 
-# function to get monitors info
+# get monitors info
 function get_monitors_info {
     function Decode {
         If ($args[0] -is [System.Array]) {
@@ -33,15 +31,34 @@ function get_monitors_info {
         }
     }
 
+    Write-Output "---------- Monitory ----------"
+
     ForEach ($Monitor in Get-WmiObject WmiMonitorID -Namespace root\wmi) {  
         $Manufacturer = Decode $Monitor.ManufacturerName -notmatch 0
         $Name = Decode $Monitor.UserFriendlyName -notmatch 0
         $Serial = Decode $Monitor.SerialNumberID -notmatch 0
         
-        Write-Output "Manufacturer : $($Manufacturer)`nModel        : $($Name)`nSerial Number: $($Serial)`n"
+        $result = "Manufacturer : $($Manufacturer)`nModel        : $($Name)`nSerial Number: $($Serial)`n".Replace("`0", "")
+        Write-Output $result
     }
 }
 
+# get current time
 function time_f {
+    Write-Output "------------ Czas ------------"
     return Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 }
+
+# save all gathered data into output.txt
+function output {
+    get_pc_info
+    get_userinfo
+    get_monitors_info
+    time_f
+}
+
+# save data into output.txt
+output | Out-File -FilePath .\output.txt
+
+
+# d-_-b
