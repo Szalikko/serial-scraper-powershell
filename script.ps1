a684f8ba89559864862f90b2e8caed97b9744332
Szalikko <91461505+Szalikko@users.noreply.github.com>
2023-09-18 17:27:39 +0200

    Create get_userinfo function
    Function to get username, domain and pcname

--- script.ps1
+++ script.ps1
@@ -1,12 +1,22 @@
 
-
-function get_pc_info() {
+# function to scrape pc info
+function get_pc_info {
     $manufacturer = (Get-WmiObject win32_bios | select Manufacturer | Format-Table -HideTableHeaders | Out-String).Trim()
     $model = (Get-CimInstance Win32_ComputerSystemProduct | Select Name | Format-Table -HideTableHeaders | Out-String).Trim()
     $serial = (Get-WmiObject win32_bios | select Serialnumber | Format-Table -HideTableHeaders | Out-String).Trim()
 
-    $result = "Manufacturer : $($manufacturer)`nModel        : $($model)`nSerial       : $($serial)`n"
-            
-        
+    $result = "Manufacturer : $($manufacturer)`nModel        : $($model)`nSerial       : $($serial)`n------------------------------"
+                
     return $result
+}
+# function to get user info
+function get_userinfo {
+    $username = ($Env:UserName).Trim()
+    $domain = ($Env:UserDomain).Trim()
+    $pcname = ($Env:ComputerName).Trim()
+
+    $result = "Username     : $($username)`nPCName       : $($pcname)`nDomain       : $($domain)`n"
+
+    return $result
+
 }
